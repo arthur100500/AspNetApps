1652526514b8f294cd72ec4c0f356d1990599ad3
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2023-11-06 14:59:25 +0300

    Add CatPhotoAPI example

---
+++ CatPhotoApi/CatPhotoApi/CatPhotoApi/Controllers/CatPhotoController.cs
@@ -0,0 +1,23 @@
+﻿using CatPhotoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatPhotoApi.Controllers;
+
+[ApiController]
+[Route("api")]
+public class CatPhotoController : ControllerBase
+{
+    [HttpPost]
+    [Route("post")]
+    public IActionResult Post([FromBody] CatPhoto p)
+    {
+        return Ok();
+    }
+    
+    [HttpGet]
+    [Route("get")]
+    public ActionResult<List<CatPhoto>> Get()
+    {
+        return new List<CatPhoto>();
+    }
+}
---
+++ CatPhotoApi/CatPhotoApi/CatPhotoApi/Models/CatPhoto.cs
@@ -0,0 +1,13 @@
+﻿namespace CatPhotoApi.Models;
+
+public class CatPhoto
+{
+    public string Url { get; set; }
+    public string CatName { get; set; }
+
+    public CatPhoto(string url, string catName)
+    {
+        Url = url;
+        CatName = catName;
+    }
+}
